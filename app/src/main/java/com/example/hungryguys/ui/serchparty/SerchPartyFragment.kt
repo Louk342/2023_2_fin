a82143a79e9d552e0e22fb6969e6cfcc29138823
Lseoksee <da864268@naver.com>
2023-11-20 14:55:27 +0900

    홈 화면 기반 잡기

---
+++ app/src/main/java/com/example/hungryguys/ui/serchparty/SerchPartyFragment.kt
@@ -0,0 +1,20 @@
+package com.example.hungryguys.ui.serchparty
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hungryguys.databinding.FragmentSerchPartyBinding
+
+class SerchPartyFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentSerchPartyBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
